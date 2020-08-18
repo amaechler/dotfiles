8194066832876f31e4479043cda8790bb82c513e
Andreas Maechler <amaechler@gmail.com>
2020-08-18 15:28:11 -0600

    Move startingDirectory to terminal

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -7,5 +7,3 @@ $script:PandellDevelopmentDir = 'C:\Users\amaechler\Development\Pandell\'
 . $PSScriptRoot\git_scripts.ps1
 . $PSScriptRoot\link_global.ps1
 . $PSScriptRoot\aliases_dev.ps1
-
-Set-Location $script:PandellDevelopmentDir
