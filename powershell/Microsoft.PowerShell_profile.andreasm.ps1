efe470fed5b32e040684deb0d3b7391dfa2dbb7b
Andreas Maechler <amaechler@gmail.com>
2021-01-18 16:43:28 -0700

    Powershell improvements
    Including usage of oh-my-posh with custom theme.

--- powershell/Microsoft.PowerShell_profile.andreasm.ps1
+++
@@ -1,9 +0,0 @@
-﻿Import-Module posh-git
-Import-Module ZLocation
-
-# Load custom scripts
-$script:PandellDevelopmentDir = 'C:\Users\andreasm\Development\Pandell\'
-
-. $PSScriptRoot\git_scripts.ps1
-. $PSScriptRoot\link_global.ps1
-. $PSScriptRoot\aliases_dev.ps1
--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,19 @@
-﻿Import-Module posh-git
-Import-Module ZLocation
+﻿Import-Module ZLocation
+
+if ((Get-Module PSReadLine).Version -ge "2.1.0") {
+    Set-PSReadLineOption -PredictionSource History # Enable "predictive intellisense" - https://devblogs.microsoft.com/powershell/announcing-psreadline-2-1-with-predictive-intellisense/
+    Set-PSReadLineKeyHandler -Chord "Ctrl+>" -Function ForwardWord
+    Set-PSReadLineKeyHandler -Chord "Ctrl+<" -Function BackwardWord
+    Set-PSReadLineOption -Colors @{ InlinePrediction = "`e[38;5;241m" }
+}
+
+Import-Module posh-git
+Invoke-Expression (oh-my-posh --init `
+        --shell pwsh `
+        --config ~\Development\GitHub\_Personal\dotfiles\oh-my-posh-amaechler.omp.json)
 
 # Load custom scripts
-$script:PandellDevelopmentDir = 'C:\Users\amaechler\Development\Pandell\'
+$script:PandellDevelopmentDir = "$HOME\Development\Pandell\"
 
 . $PSScriptRoot\git_scripts.ps1
 . $PSScriptRoot\link_global.ps1
