52b0c96dac9afc7b0d225dfc28a005214c8ad77b
Andreas Maechler <amaechler@gmail.com>
2023-07-30 08:58:05 -0600

    Small tweaks over the year

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,14 @@
 if status is-interactive
     # volta (node / npm manager) configuration
     export VOLTA_HOME="$HOME/.volta"
-    export PATH="$VOLTA_HOME/bin:$PATH"
+    fish_add_path $VOLTA_HOME/bin
 
     # execute oh-my-posh prompt
     oh-my-posh init fish --config ~/Development/Personal/dotfiles/oh-my-posh/oh-my-posh-amaechler.omp.json | source
 
     # autojump
     [ -f $(brew --prefix)/share/autojump/autojump.fish ]; and source $(brew --prefix)/share/autojump/autojump.fish
+
+    # add python executables to path
+    fish_add_path /opt/homebrew/opt/python/libexec/bin
 end
